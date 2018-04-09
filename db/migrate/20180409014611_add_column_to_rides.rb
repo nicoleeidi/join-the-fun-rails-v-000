ba11e1c9c9b6c49161c95aa36f2c876eb2eaf2ea
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-09 01:47:01 +0000

    automatically backed up by learn

--- db/migrate/20180409014611_add_column_to_rides.rb
+++ db/migrate/20180409014611_add_column_to_rides.rb
@@ -1,5 +1,5 @@
 class AddColumnToRides < ActiveRecord::Migration
   def change
-    add_column :rides, :passenger_id, :integer 
-    add_column :rides, :taxi_id, :integer s  end
+    add_column :rides, :passenger_id, :integer
+    add_column :rides, :taxi_id, :integer
 end
